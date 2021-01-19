50fceb18cb1df63c5d6e415d441cfde7f48ddb0f
Sandro Andrade <sandro.andrade@nitryx.com>
2021-01-19 20:45:32 -0300

    Criando tela de lançamento

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule} from 'primeng/components/button/button'
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
